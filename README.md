0c03956dc9c8cb80017a67bd6985c2f51e83d758
Hichem ELB <hi.boug@gmail.com>
2022-06-07 00:14:39 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,9 @@
+# target-amplitude-batch
+
+This is a [Singer](https://singer.io) target that reads JSON-formatted data pulled from BigQuery and send it to Amplitude Batch API
+following the [Singer spec](https://github.com/singer-io/getting-started/blob/master/SPEC.md).
+
+
+---
+
+Copyright &copy; 2022 DTSL
---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+VERSION = "1.0.0"
+
+setup(
+    name="target-amplitude-batch",
+    version=VERSION,
+    description="Singer.io target for loading data to Amplitude Batch / Identify APIs",
+    author="HichemELB",
+    url="git@github.com:DTSL/target-amplitude-batch.git",
+    classifiers=["Programming Language :: Python :: 3 :: Only"],
+    py_modules=["target_amplitude_batch"],
+    install_requires=[
+        "singer-python==5.12.2",
+        "adjust-precision-for-schema==0.3.3",
+        "jsonschema==2.6.0",
+        "amplitude-analytics==0.3.0",
+    ],
+    entry_points="""
+    [console_scripts]
+    target-amplitude-batch=target_amplitude_batch:main
+    """,
+)
